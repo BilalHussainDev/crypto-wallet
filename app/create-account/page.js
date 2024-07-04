39d66c7f24493da3c73fdbf277628eeb89ce9ac7
BilalHussain <hellobilalhussain@gmail.com>
2024-07-04 11:53:29 +0500

    Create Function to Store Key

--- app/create-account/page.js
+++ app/create-account/page.js
@@ -3,12 +3,11 @@
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { generateMnemonic, getAccountFromMnemonic } from "@/utils/mnemonic";
-import { getWeb3 } from "@/utils/web3";
+import { storeMnemonic } from "@/utils/storage";
 
 const CreateAccount = () => {
   const [password, setPassword] = useState("");
   const [mnemonic, setMnemonic] = useState("");
-  const [showMnemonic, setShowMnemonic] = useState(false);
 
   const router = useRouter();
 
@@ -16,25 +15,10 @@ const CreateAccount = () => {
     e.preventDefault();
     // generate mnemonic
     const generatedMnemonic = generateMnemonic();
-    // show mnemonic to user
+    // remember mnemonic
     setMnemonic(generatedMnemonic);
-    // get privatekey from mnemonic
-    const { privateKey } = getAccountFromMnemonic(generatedMnemonic);
-    // encrypt private key
-    const web3 = await getWeb3();
-    const encryptedKey = await web3.eth.accounts.encrypt(
-      `0x${privateKey}`,
-      password,
-      {
-        iv: "bfb43120ae00e9de110f8325143a2709",
-        salt: "210d0ec956787d865358ac45716e6dd42e68d48e346d795746509523aeb477dd",
-        c: 262144,
-        kdf: "pbkdf2",
-      }
-    );
-    // save encrypted key to local storage
-    localStorage.setItem("encryptedKey", encryptedKey);
-    setShowMnemonic(true);
+    // Encrypt and Store mnemonic in local storage
+    storeMnemonic(mnemonic, password);
   };
 
   const handleDone = () => {
@@ -45,7 +29,7 @@ const CreateAccount = () => {
   return (
     <div>
       <h1>Create Account</h1>
-      {!showMnemonic && (
+      {!mnemonic ? (
         <form onSubmit={handlePasswordSubmit}>
           <label htmlFor="password">Enter a password </label>
           <input
@@ -57,9 +41,7 @@ const CreateAccount = () => {
           />
           <button type="submit">Submit</button>
         </form>
-      )}
-
-      {showMnemonic && (
+      ) : (
         <div>
           <p>
             Note these words, as there is no way of restoring it after you press
--- app/restore-account/page.js
+++ app/restore-account/page.js
@@ -6,6 +6,7 @@ import { getAccountFromMnemonic } from "@/utils/mnemonic";
 
 const RestoreAccount = () => {
   const [mnemonic, setMnemonic] = useState("");
+  const [password, setPassword] = useState("");
   const router = useRouter();
 
   const handleMnemonicSubmit = (e) => {
@@ -18,15 +19,26 @@ const RestoreAccount = () => {
     <div>
       <h1>Restore Account</h1>
       <form onSubmit={handleMnemonicSubmit}>
-        <label>
-          Enter your mnemonic phrase:
+        <p>
+          <label htmlFor="phrase">Enter your secret phrase: </label>
           <input
             type="text"
+            id="phrase"
             value={mnemonic}
             onChange={(e) => setMnemonic(e.target.value)}
             required
           />
-        </label>
+        </p>
+        <p>
+          <label htmlFor="password">Create a Password: </label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </p>
         <button type="submit">Restore Account</button>
       </form>
     </div>
