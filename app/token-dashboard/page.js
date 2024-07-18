4cf8139ddd84e43260b9bc4dbd8e75c7d922422a
BilalHussainDev <hellobilalhussain@gmail.com>
2024-07-18 23:10:02 +0500

    Create Token Dashboard

---
+++ app/token-dashboard/page.js
@@ -0,0 +1,119 @@
+"use client";
+
+import { Suspense } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { useSearchParams } from "next/navigation";
+import { Box, Button, Typography } from "@mui/material";
+
+import { ActivityTab } from "@/components";
+
+function DashboardComponent() {
+	const searchParams = useSearchParams();
+	const address = searchParams.get("address");
+	const balance = +searchParams.get("balance");
+	const symbol = searchParams.get("symbol");
+
+	if (!address) {
+		return <p>No account selected.</p>;
+	}
+
+	return (
+		<Box component="section">
+			<Typography
+				sx={{
+					whiteSpace: "nowrap",
+					overflow: "hidden",
+					textOverflow: "ellipsis",
+					textAlign: "center",
+					fontSize: "14px",
+					pt: "16px",
+				}}
+			>
+				{address}
+			</Typography>
+
+			<Box>
+				<Image
+					src="/img/bobbin.png"
+					alt="ETH"
+					width={100}
+					height={100}
+					priority
+				/>
+			</Box>
+
+			<Typography sx={{ fontSize: "18px", fontWeight: "600" }}>
+				{balance.toFixed(4)} {symbol}
+			</Typography>
+
+			<Box
+				sx={{
+					margin: "1.5rem 0",
+					display: "flex",
+					justifyContent: "space-evenly",
+				}}
+			>
+				<Button
+					sx={{ width: "96px", padding: "0", height: "2rem" }}
+					variant="outlined"
+				>
+					<Link
+						href={`/send-tokens?address=${address}`}
+						style={{ width: "100%" }}
+					>
+						Send
+					</Link>
+				</Button>
+				<Button
+					sx={{ width: "96px", padding: "0", height: "2rem" }}
+					variant="outlined"
+				>
+					<Link
+						href={`/receive-funds?address=${address}`}
+						style={{ width: "100%" }}
+					>
+						Receive
+					</Link>
+				</Button>
+				<Button
+					sx={{ width: "96px", padding: "0", height: "2rem" }}
+					variant="outlined"
+				>
+					<Link href={`/unlock-wallet`} style={{ width: "100%" }}>
+						Logout
+					</Link>
+				</Button>
+			</Box>
+
+			<Typography
+				color="primary"
+				sx={{
+					mb: "1.5rem",
+					fontSize: "25px",
+					fontWeight: "500",
+					lineHeight: "0.78",
+					textAlign: "center",
+				}}
+			>
+				Activity
+			</Typography>
+
+			<ActivityTab address={address} />
+		</Box>
+	);
+}
+
+export default function TokenDashboardPage() {
+	return (
+		<Suspense
+			fallback={
+				<Box>
+					<Typography>Loading.....</Typography>
+				</Box>
+			}
+		>
+			<DashboardComponent />
+		</Suspense>
+	);
+}
--- components/ImportToken.js
+++ components/ImportToken.js
@@ -1,168 +1,169 @@
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { useFormik } from "formik";
 import { object, string } from "yup";
 import {
-  Box,
-  Button,
-  FormControl,
-  FormHelperText,
-  OutlinedInput,
-  Typography,
+	Box,
+	Button,
+	FormControl,
+	FormHelperText,
+	OutlinedInput,
+	Typography,
 } from "@mui/material";
 
 import { ButtonLoader } from ".";
 import { getTokenDetails, storeToken } from "@/utils/token";
-import { useRouter } from "next/navigation";
 
 const addressRules = /^0x[a-fA-F0-9]{40}$/;
 
 const ImportToken = ({ address }) => {
-  const [symbol, setSymbol] = useState("Token symbol");
-  const [decimals, setDecimals] = useState("Token decimals");
+	const [symbol, setSymbol] = useState("Token symbol");
+	const [decimals, setDecimals] = useState("Token decimals");
 
-  const router = useRouter();
+	const router = useRouter();
 
-  // function for yup validation test of token address
-  async function isTokenValid(address) {
-    const res = await getTokenDetails(address);
-    if (res.ok) {
-      setSymbol(`Token Symbol: ${res.data.symbol}`);
-      setDecimals(`Token Decimals: ${res.data.decimals}`);
-      return true;
-    }
-    setSymbol("Token symbol");
-    setDecimals("Token decimals");
-    return false;
-  }
+	// function for yup validation test of token address
+	async function isTokenValid(address) {
+		const res = await getTokenDetails(address);
+		if (res.ok) {
+			setSymbol(`Token Symbol: ${res.data.symbol}`);
+			setDecimals(`Token Decimals: ${res.data.decimals}`);
+			return true;
+		}
+		setSymbol("Token symbol");
+		setDecimals("Token decimals");
+		return false;
+	}
 
-  // schema for reset password form or like that
-  const formSchema = object({
-    address: string()
-      .matches(addressRules, {
-        message: "Invalid contract address",
-      })
-      .required("Contract address is required")
-      .test("is-token-valid", "Invalid contract address", isTokenValid),
-  });
+	// schema for reset password form or like that
+	const formSchema = object({
+		address: string()
+			.matches(addressRules, {
+				message: "Invalid contract address",
+			})
+			.required("Contract address is required")
+			.test("is-token-valid", "Invalid contract address", isTokenValid),
+	});
 
-  async function handleTokenSubmit(data, actions) {
-    // Simulate a delay
-    await new Promise((resolve) => setTimeout(resolve, 500));
+	async function handleTokenSubmit(data, actions) {
+		// Simulate a delay
+		await new Promise((resolve) => setTimeout(resolve, 500));
 
-    // store token address in local storage
-    storeToken(address, data.address);
-    console.log('inside submit')
-    
-    // redirect to dashboard with account address
-    router.push(`/dashboard?address=${address}`);
-    actions.resetForm();
-  }
+		// store token address in local storage
+		storeToken(address, data.address);
 
-  // Extracting Form State and Helper Methods from formik
-  const {
-    values,
-    errors,
-    touched,
-    isSubmitting,
-    handleChange,
-    handleBlur,
-    handleSubmit,
-  } = useFormik({
-    initialValues: {
-      address: "",
-    },
-    validationSchema: formSchema,
-    onSubmit: handleTokenSubmit,
-  });
+		// redirect to dashboard with account address
+		router.push(`/dashboard?address=${address}`);
+		actions.resetForm();
+	}
 
-  return (
-    <>
-      <Typography
-        variant="body2"
-        type="button"
-        color="primary"
-        textAlign="center"
-        padding="0.5rem 0"
-        sx={{ fontSize: "2.5rem", textAlign: "left" }}
-      >
-        <Link href={`/dashboard?address=${address}`}>⬅</Link>
-      </Typography>
+	// Extracting Form State and Helper Methods from formik
+	const {
+		values,
+		errors,
+		touched,
+		isSubmitting,
+		handleChange,
+		handleBlur,
+		handleSubmit,
+	} = useFormik({
+		initialValues: {
+			address: "",
+		},
+		validationSchema: formSchema,
+		onSubmit: handleTokenSubmit,
+	});
 
-      <Typography
-        component="h1"
-        variant="h5"
-        sx={{ mb: "2rem", fontWeight: "bold" }}
-      >
-        Import Tokens
-      </Typography>
+	return (
+		<>
+			<Typography
+				variant="body2"
+				type="button"
+				color="primary"
+				textAlign="center"
+				padding="0.5rem 0"
+				sx={{ fontSize: "2.5rem", textAlign: "left" }}
+			>
+				<Link href={`/dashboard?address=${address}`}>⬅</Link>
+			</Typography>
 
-      <Box component="form" onSubmit={handleSubmit} autoComplete="off">
-        <FormControl fullWidth sx={{ minHeight: "80px" }}>
-          <OutlinedInput
-            name="address"
-            placeholder="Enter token contract address"
-            value={values.address}
-            onChange={handleChange}
-            onBlur={handleBlur}
-            disabled={isSubmitting}
-            error={errors.address && touched.address ? true : false}
-          />
-          <FormHelperText sx={{ color: "red" }}>
-            {errors.address && touched.address ? errors.address : ""}
-          </FormHelperText>
-        </FormControl>
+			<Typography
+				component="h1"
+				variant="h5"
+				sx={{ mb: "2rem", fontWeight: "bold" }}
+			>
+				Import Tokens
+			</Typography>
 
-        <Box>
-          <Typography
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              padding: "16.5px 14px",
-              height: "56px",
-              color: "rgba(0, 0, 0, 0.35)",
-              border: "1px solid rgba(0, 0, 0, 0.23)",
-              borderRadius: "4px",
-              marginBottom: "24px",
-            }}
-          >
-            {symbol}
-          </Typography>
-        </Box>
+			<Box component="form" onSubmit={handleSubmit} autoComplete="off">
+				<FormControl fullWidth sx={{ minHeight: "80px" }}>
+					<OutlinedInput
+						name="address"
+						placeholder="Enter token contract address"
+						value={values.address}
+						onChange={handleChange}
+						onBlur={handleBlur}
+						disabled={isSubmitting}
+						error={errors.address && touched.address ? true : false}
+					/>
+					<FormHelperText sx={{ color: "red" }}>
+						{errors.address && touched.address
+							? errors.address
+							: ""}
+					</FormHelperText>
+				</FormControl>
 
-        <Box>
-          <Typography
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              padding: "16.5px 14px",
-              height: "56px",
-              color: "rgba(0, 0, 0, 0.35)",
-              border: "1px solid rgba(0, 0, 0, 0.23)",
-              borderRadius: "4px",
-              marginBottom: "24px",
-            }}
-          >
-            {decimals}
-          </Typography>
-        </Box>
+				<Box>
+					<Typography
+						sx={{
+							display: "flex",
+							alignItems: "center",
+							padding: "16.5px 14px",
+							height: "56px",
+							color: "rgba(0, 0, 0, 0.35)",
+							border: "1px solid rgba(0, 0, 0, 0.23)",
+							borderRadius: "4px",
+							marginBottom: "24px",
+						}}
+					>
+						{symbol}
+					</Typography>
+				</Box>
 
-        {isSubmitting ? (
-          <ButtonLoader>Adding.....</ButtonLoader>
-        ) : (
-          <Button
-            fullWidth
-            type="submit"
-            variant="contained"
-            disabled={isSubmitting}
-            sx={{ height: "40px" }}
-          >
-            Add Custom Token
-          </Button>
-        )}
-      </Box>
-    </>
-  );
+				<Box>
+					<Typography
+						sx={{
+							display: "flex",
+							alignItems: "center",
+							padding: "16.5px 14px",
+							height: "56px",
+							color: "rgba(0, 0, 0, 0.35)",
+							border: "1px solid rgba(0, 0, 0, 0.23)",
+							borderRadius: "4px",
+							marginBottom: "24px",
+						}}
+					>
+						{decimals}
+					</Typography>
+				</Box>
+
+				{isSubmitting ? (
+					<ButtonLoader>Adding Token.....</ButtonLoader>
+				) : (
+					<Button
+						fullWidth
+						type="submit"
+						variant="contained"
+						disabled={isSubmitting}
+						sx={{ height: "40px" }}
+					>
+						Add Custom Token
+					</Button>
+				)}
+			</Box>
+		</>
+	);
 };
 
 export default ImportToken;
