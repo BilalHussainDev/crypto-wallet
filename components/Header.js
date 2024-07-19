171ac51328d9890e544bdac8893de2bc7d1fd4ed
BilalHussain <hellobilalhussain@gmail.com>
2024-07-19 15:34:56 +0500

    Unpdate Header

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,5 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Tooltip, Typography } from "@mui/material";
+import LogoutRoundedIcon from "@mui/icons-material/LogoutRounded";
 import Link from "next/link";
 
 export default function Header() {
@@ -8,24 +9,9 @@ export default function Header() {
         display: "flex",
         justifyContent: "space-between",
         alignItems: "center",
-        padding: "8px 24px",
+        padding: "8px 20px",
       }}
     >
-      <Box
-        sx={{
-          height: "2rem",
-          border: "1px solid #0459ad",
-          borderRadius: "18px",
-          display: "flex",
-          alignItems: "center",
-          padding: "0 20px",
-          color: "#0459ad",
-        }}
-      >
-        <Typography>
-          <Link href="/">Crypto Wallet</Link>
-        </Typography>
-      </Box>
       <Box
         sx={{
           width: "2rem",
@@ -43,6 +29,45 @@ export default function Header() {
           ETH
         </Typography>
       </Box>
+
+      <Box
+        sx={{
+          height: "2rem",
+          border: "1px solid #0459ad",
+          borderRadius: "18px",
+          display: "flex",
+          alignItems: "center",
+          padding: "0 16px",
+          color: "#0459ad",
+        }}
+      >
+        <Typography fontSize={14}>Polygon Amoy Testnet</Typography>
+      </Box>
+
+      {/* <Box
+        sx={{
+          width: "2rem",
+          height: "2rem",
+          borderRadius: "50%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          backgroundColor: "#0c668f",
+        }}
+      >
+        <Tooltip title="Logout" placement="bottom">
+          <Link href="/unlock-wallet">
+            <LogoutRoundedIcon
+              fontSize="2rem"
+              sx={{
+                transform: "translate(2px, 2px)",
+                color: "#c7dbdf",
+                fontSize: "16px",
+              }}
+            />
+          </Link>
+        </Tooltip>
+      </Box> */}
     </Box>
   );
 }
