9faa7d94c2c91ec1e72cfa31149696ed6d2dc26a
BilalHussain <hellobilalhussain@gmail.com>
2024-07-22 10:01:39 +0500

    Improve Balance Loading

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -4,7 +4,7 @@ import { Suspense, useEffect, useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { useSearchParams } from "next/navigation";
-import { Box, Button, CircularProgress, Typography } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
 
 import { getBalance } from "@/utils/account";
 import { DashboardTabs } from "@/components";
@@ -27,91 +27,99 @@ function DashboardComponent() {
 
   return (
     <>
-    <Box component="section">
-      <Typography
-        sx={{
-          whiteSpace: "nowrap",
-          overflow: "hidden",
-          textOverflow: "ellipsis",
-          textAlign: "center",
-          fontSize: "14px",
-          pt: "16px",
-        }}
-      >
-        {address}
-      </Typography>
-
-      <Box>
-        <Image
-          src="/img/ethereum.png"
-          alt="ETH"
-          width={100}
-          height={100}
-          priority
-        />
-      </Box>
-
-      <Typography sx={{ fontSize: "18px", fontWeight: "600" }}>
-        {balance.toFixed(4)} ETH
-      </Typography>
-
-      <Box
-        sx={{
-          margin: "1.5rem 0",
-          display: "flex",
-          justifyContent: "space-evenly",
-        }}
+      <Box component="section">
+        <Typography
+          sx={{
+            whiteSpace: "nowrap",
+            overflow: "hidden",
+            textOverflow: "ellipsis",
+            textAlign: "center",
+            fontSize: "14px",
+            pt: "16px",
+          }}
         >
-        <Button
-          sx={{ width: "96px", padding: "0", height: "2rem" }}
-          variant="outlined"
+          {address}
+        </Typography>
+
+        <Box>
+          <Image
+            src="/img/ethereum.png"
+            alt="ETH"
+            width={100}
+            height={100}
+            priority
+          />
+        </Box>
+
+        <Box sx={{display: 'flex', justifyContent: 'center', height: '28px' }}>
+        {!balance ? (
+          <div class="loader"></div>
+        ) : (
+          <Typography sx={{ fontSize: "18px", fontWeight: "600" }}>
+            {balance.toFixed(4)} ETH
+          </Typography>
+        )}
+        </Box>
+
+        <Box
+          sx={{
+            margin: "1.5rem 0",
+            display: "flex",
+            justifyContent: "space-evenly",
+          }}
+        >
+          <Button
+            sx={{ width: "96px", padding: "0", height: "2rem" }}
+            variant="outlined"
           >
-          <Link
-            href={`/send-funds?address=${address}`}
-            style={{ width: "100%" }}
+            <Link
+              href={`/send-funds?address=${address}`}
+              style={{ width: "100%" }}
             >
-            Send
-          </Link>
-        </Button>
-        <Button
-          sx={{ width: "96px", padding: "0", height: "2rem" }}
-          variant="outlined"
+              Send
+            </Link>
+          </Button>
+          <Button
+            sx={{ width: "96px", padding: "0", height: "2rem" }}
+            variant="outlined"
           >
-          <Link
-            href={`/receive-funds?address=${address}`}
-            style={{ width: "100%" }}
+            <Link
+              href={`/receive-funds?address=${address}`}
+              style={{ width: "100%" }}
             >
-            Receive
-          </Link>
-        </Button>
-        <Button
-          sx={{ width: "96px", padding: "0", height: "2rem" }}
-          variant="outlined"
+              Receive
+            </Link>
+          </Button>
+          <Button
+            sx={{ width: "96px", padding: "0", height: "2rem" }}
+            variant="outlined"
           >
-          <Link href={`/unlock-wallet`} style={{ width: "100%" }}>
-            Logout
-          </Link>
-        </Button>
+            <Link href={`/unlock-wallet`} style={{ width: "100%" }}>
+              Logout
+            </Link>
+          </Button>
+        </Box>
+
+        <DashboardTabs address={address} />
       </Box>
 
-      <DashboardTabs address={address}/>
-    </Box>
-
-    {!balance && <Box sx={{
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-      position: 'absolute',
-      top: '0',
-      zIndex: '100',
-      backdropFilter:'blur(5px)',
-      width: '100%',
-      height: '100%'
-      }}>
-        <CircularProgress />
-    </Box>
-  }
-</>
+      {/* Disable all interactions while loading */}
+      {!balance && (
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            position: "absolute",
+            top: "0",
+            zIndex: "100",
+            width: "100%",
+            height: "100%",
+          }}
+        >
+        </Box>
+      )}
+    </>
   );
 }
 
--- app/token-dashboard/page.js
+++ app/token-dashboard/page.js
@@ -29,98 +29,122 @@ function DashboardComponent() {
 	}
 
 	return (
-    <Box component="section">
-      <Typography
-        variant="body2"
-        type="button"
-        color="primary"
-        textAlign="center"
-        padding="0"
-        sx={{ fontSize: "2.5rem", textAlign: "left" }}
-      >
-        <Link href={`/dashboard?address=${address}`}>⬅</Link>
-      </Typography>
+    <>
+      <Box component="section">
+        <Typography
+          variant="body2"
+          type="button"
+          color="primary"
+          textAlign="center"
+          padding="0"
+          sx={{ fontSize: "2.5rem", textAlign: "left" }}
+        >
+          <Link href={`/dashboard?address=${address}`}>⬅</Link>
+        </Typography>
 
-      <Typography
-        sx={{
-          whiteSpace: "nowrap",
-          overflow: "hidden",
-          textOverflow: "ellipsis",
-          textAlign: "center",
-          fontSize: "14px",
-        }}
-      >
-        {address}
-      </Typography>
+        <Typography
+          sx={{
+            whiteSpace: "nowrap",
+            overflow: "hidden",
+            textOverflow: "ellipsis",
+            textAlign: "center",
+            fontSize: "14px",
+          }}
+        >
+          {address}
+        </Typography>
 
-      <Box>
-        <Image
-          src="/img/bobbin.png"
-          alt="ETH"
-          width={100}
-          height={100}
-          priority
-        />
+        <Box>
+          <Image
+            src="/img/bobbin.png"
+            alt="ETH"
+            width={100}
+            height={100}
+            priority
+          />
+        </Box>
+
+        <Box sx={{ display: "flex", justifyContent: "center", height: "28px" }}>
+          {!balance ? (
+            <div class="loader"></div>
+          ) : (
+            <Typography sx={{ fontSize: "18px", fontWeight: "600" }}>
+              {balance.toFixed(4)} {symbol}
+            </Typography>
+          )}
+        </Box>
+
+        <Box
+          sx={{
+            margin: "1.5rem 0",
+            display: "flex",
+            justifyContent: "space-evenly",
+          }}
+        >
+          <Button
+            sx={{ width: "96px", padding: "0", height: "2rem" }}
+            variant="outlined"
+          >
+            <Link
+              href={`/send-tokens?address=${address}&tokenAddress=${tokenAddress}&symbol=${symbol}&balance=${balance}`}
+              style={{ width: "100%" }}
+            >
+              Send
+            </Link>
+          </Button>
+          <Button
+            sx={{ width: "96px", padding: "0", height: "2rem" }}
+            variant="outlined"
+          >
+            <Link
+              href={`/receive-funds?address=${address}`}
+              style={{ width: "100%" }}
+            >
+              Receive
+            </Link>
+          </Button>
+          <Button
+            sx={{ width: "96px", padding: "0", height: "2rem" }}
+            variant="outlined"
+          >
+            <Link href={`/unlock-wallet`} style={{ width: "100%" }}>
+              Logout
+            </Link>
+          </Button>
+        </Box>
+
+        <Typography
+          color="primary"
+          sx={{
+            mb: "1.5rem",
+            fontSize: "25px",
+            fontWeight: "500",
+            lineHeight: "0.78",
+            textAlign: "center",
+          }}
+        >
+          Activity
+        </Typography>
+
+        <ActivityTab address={address} />
       </Box>
 
-      <Typography sx={{ fontSize: "18px", fontWeight: "600" }}>
-        {balance.toFixed(4)} {symbol}
-      </Typography>
-
-      <Box
-        sx={{
-          margin: "1.5rem 0",
-          display: "flex",
-          justifyContent: "space-evenly",
-        }}
-      >
-        <Button
-          sx={{ width: "96px", padding: "0", height: "2rem" }}
-          variant="outlined"
-        >
-          <Link
-            href={`/send-tokens?address=${address}&tokenAddress=${tokenAddress}&symbol=${symbol}&balance=${balance}`}
-            style={{ width: "100%" }}
-          >
-            Send
-          </Link>
-        </Button>
-        <Button
-          sx={{ width: "96px", padding: "0", height: "2rem" }}
-          variant="outlined"
-        >
-          <Link
-            href={`/receive-funds?address=${address}`}
-            style={{ width: "100%" }}
-          >
-            Receive
-          </Link>
-        </Button>
-        <Button
-          sx={{ width: "96px", padding: "0", height: "2rem" }}
-          variant="outlined"
-        >
-          <Link href={`/unlock-wallet`} style={{ width: "100%" }}>
-            Logout
-          </Link>
-        </Button>
-      </Box>
-
-      <Typography
-        color="primary"
-        sx={{
-          mb: "1.5rem",
-          fontSize: "25px",
-          fontWeight: "500",
-          lineHeight: "0.78",
-          textAlign: "center",
-        }}
-      >
-        Activity
-      </Typography>
-
-      <ActivityTab address={address} />
-    </Box>
+      {/* Disable all interactions while loading */}
+      {!balance && (
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            position: "absolute",
+            top: "0",
+            zIndex: "100",
+            width: "100%",
+            height: "100%",
+          }}
+        ></Box>
+      )}
+    </>
   );
 }
 
